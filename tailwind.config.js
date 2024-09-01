6bc28cb694c9e4e4a7a69a0fde154aff2c925d89
Erick Nascimento <eriknascimento73@gmail.com>
2024-09-01 03:18:11 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/public/views/**/*.{html,js}'],
-  purge: ['./src/**/*.{js,html}'],
+  purge: ['./src/public/views/**/*.{js,html}'],
   theme: {
     extend: {},
   },
